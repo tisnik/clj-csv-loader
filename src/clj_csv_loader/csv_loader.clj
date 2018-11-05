b195c732043e03f076bb546e087146bd23acf526
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-11-05 10:19:13 +0100

    CSV loader implementation

--- src/clj_csv_loader/csv_loader.clj
+++ src/clj_csv_loader/csv_loader.clj
@@ -1,6 +1,37 @@
-(ns clj-csv-loader.core)
+;
+;  (C) Copyright 2017, 2018  Pavel Tisnovsky
+;
+;  All rights reserved. This program and the accompanying materials
+;  are made available under the terms of the Eclipse Public License v1.0
+;  which accompanies this distribution, and is available at
+;  http://www.eclipse.org/legal/epl-v10.html
+;
+;  Contributors:
+;      Pavel Tisnovsky
+;
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(ns chainring-service.csv-loader)
+
+(require '[clojure.data.csv :as csv])
+(require '[clojure.java.io  :as io])
+
+(defn csv-data->maps
+    [csv-data]
+    (map zipmap
+        (->> (first csv-data)  ;; header
+             (map keyword)     ;; heder items -> keywords
+             repeat)
+             (rest csv-data)))
+
+(defn load-csv
+    [filename]
+    (with-open [reader (io/reader filename)]
+        (let [data (csv/read-csv reader)]
+             (doall (csv-data->maps data)))))
+
+(defn load-csv-for-all-dates
+    [dates-from data-directory filename]
+    (zipmap dates-from
+        (for [date dates-from]
+            (let [full-filename (str data-directory "/" date "/" filename)]
+                (load-csv full-filename)))))
