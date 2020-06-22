380b3be972184f081395efa73e7abe8fc7f4ba69
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-06-22 10:09:07 +0200

    Reformatted project.clj by zprint

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 ;
-;  (C) Copyright 2018, 2020  Pavel Tisnovsky
+;  (C) Copyright 2018, 2019, 2020  Pavel Tisnovsky
 ;
 ;  All rights reserved. This program and the accompanying materials
 ;  are made available under the terms of the Eclipse Public License v1.0
@@ -11,12 +11,12 @@
 ;
 
 (defproject org.clojars.tisnik/clj-csv-loader "0.1.0-SNAPSHOT"
-    :description "Set of helper functions for loading CSV files."
-    :url "http://example.com/FIXME"
-    :license {:name "Eclipse Public License"
-              :url "http://www.eclipse.org/legal/epl-v10.html"}
-    :dependencies [[org.clojure/clojure "1.8.0"]
-                   [org.clojure/data.csv  "0.1.2"]]
-    :plugins [[test2junit "1.1.0"]
-              [codox "0.8.11"]
-              [lein-cloverage "1.0.7-SNAPSHOT"]])
+  :description "Set of helper functions for loading CSV files."
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License",
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/data.csv "0.1.2"]]
+  :plugins [[test2junit "1.1.0"]
+            [codox "0.8.11"]
+            [lein-cloverage "1.0.7-SNAPSHOT"]])
