4c19fda680395dde5fdcfd2eed4d418e3e1fad08
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-06-22 10:10:15 +0200

    Reformatted csv_loader.clj by zprint

--- src/clj_csv_loader/csv_loader.clj
+++ src/clj_csv_loader/csv_loader.clj
@@ -16,25 +16,25 @@
 (require '[clojure.java.io  :as io])
 
 (defn csv-data->maps
-    "Convert read CSV data into proper Clojure map."
-    [csv-data]
-    (map zipmap
-        (->> (first csv-data)  ;; header
-             (map keyword)     ;; heder items -> keywords
-             repeat)
-             (rest csv-data)))
+  "Convert read CSV data into proper Clojure map."
+  [csv-data]
+  (map zipmap
+    (->> (first csv-data)  ;; header
+         (map keyword)     ;; heder items -> keywords
+         repeat)
+    (rest csv-data)))
 
 (defn load-csv
-    "Load CSV specified by filename and convert it to a proper Clojure map."
-    [filename]
-    (with-open [reader (io/reader filename)]
-        (let [data (csv/read-csv reader)]
-             (doall (csv-data->maps data)))))
+  "Load CSV specified by filename and convert it to a proper Clojure map."
+  [filename]
+  (with-open [reader (io/reader filename)]
+    (let [data (csv/read-csv reader)]
+         (doall (csv-data->maps data)))))
 
 (defn load-csv-for-all-dates
-    "Load CSV files for all dates specified."
-    [dates-from data-directory filename]
-    (zipmap dates-from
-        (for [date dates-from]
+  "Load CSV files for all dates specified."
+  [dates-from data-directory filename]
+  (zipmap dates-from
+          (for [date dates-from]
             (let [full-filename (str data-directory "/" date "/" filename)]
-                (load-csv full-filename)))))
+              (load-csv full-filename)))))
